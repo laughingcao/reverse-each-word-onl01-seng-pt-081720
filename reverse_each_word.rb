999e2b65aee70ffd6a3773e5559a8a952eabaf7d
William T Cao <williamacao@gmail.com>
2020-08-28 19:47:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,4 @@
 def reverse_each_word(sentence)
-  sentence.split.map {|word| word.reverse}.join(" ")
+  sentence.collect {|word| word.reverse}.join(" ")
 end
 
-def reverse_each_word(sentence)
-  sentence.collect {|word| word.reverse|}
-end
